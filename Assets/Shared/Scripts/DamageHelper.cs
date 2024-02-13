7a8d214e29655d3b779a2780b90b896319b24fd9
r Star <starlove124760@gmail.com>
2024-02-13 13:23:31 +0800

    add DrawCast for DamageHelper

--- Assets/Shared/Scripts/DamageHelper.cs
+++ Assets/Shared/Scripts/DamageHelper.cs
@@ -1,7 +1,9 @@
 #region
 
 using System.Linq;
+using Drawing;
 using rStarUtility.Util.Extensions.Csharp;
+using rStarUtility.Util.Extensions.Unity;
 using UnityEngine;
 
 #endregion
@@ -14,9 +16,23 @@ namespace Shared.Scripts
 
         public static void DealDamage(Vector2 position , Vector2 range)
         {
+            DrawCast(position , range);
             var damageables = CastHelper.Cast(position , range);
             Debug.Log($"damageables count: {damageables.Count()}");
             damageables.ForEach(damageable => Debug.Log($"{damageable}"));
+        }
+
+    #endregion
+
+    #region Private Methods
+
+        private static void DrawCast(Vector2 position , Vector2 size)
+        {
+            var draw = Draw.ingame;
+            using (draw.WithColor(Color.magenta))
+            {
+                draw.SolidBox(position.ToVector3() , size.ToVector3());
+            }
         }
 
     #endregion
